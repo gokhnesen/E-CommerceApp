fa24c7baec310f4dd6709a598d33d9d839f3ce4d
gokhnesen <gokhan_esen@hotmail.com.tr>
2023-04-07 16:26:21 +0300

    Creating API basics

--- ECommerceApp/ECommerceApp.API/ECommerceApp.API/Program.cs
+++ ECommerceApp/ECommerceApp.API/ECommerceApp.API/Program.cs
@@ -1,4 +1,7 @@
 
+using ECommerceApp.Persistance.Context;
+using Microsoft.EntityFrameworkCore;
+
 namespace ECommerceApp.API
 {
     public class Program
@@ -7,23 +10,21 @@ namespace ECommerceApp.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<StoreContext>(opt =>
+            {
+                opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-
-            app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
---
+++ ECommerceApp/ECommerceApp.API/ECommerceApp.Persistance/Context/StoreContext.cs
@@ -0,0 +1,20 @@
+﻿using ECommerceApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp.Persistance.Context
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options):base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
