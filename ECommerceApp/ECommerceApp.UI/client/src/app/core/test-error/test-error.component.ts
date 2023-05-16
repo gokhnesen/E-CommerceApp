418125089917d792885a7461b3419d0f8e7bc1c9
gokhnesen <gokhan_esen@hotmail.com.tr>
2023-05-16 14:13:58 +0300

    Creating http interceptors and error components

--- ECommerceApp/ECommerceApp.UI/client/src/app/core/test-error/test-error.component.ts
+++ ECommerceApp/ECommerceApp.UI/client/src/app/core/test-error/test-error.component.ts
@@ -9,6 +9,7 @@ import { environment } from 'src/environments/environment';
 })
 export class TestErrorComponent {
   baseUrl = environment.apiUrl
+  validationErrors: string[] = [];
 
   constructor(
     private http:HttpClient
@@ -40,7 +41,9 @@ export class TestErrorComponent {
   get400ValidationError(){
     this.http.get(this.baseUrl + 'product/fortytwo').subscribe({
       next: response => console.log(response),
-      error: error => console.log(error)
+      error: error => {;
+      this.validationErrors = error.errors;
+    }
     })
   }
 }
